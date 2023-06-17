bf07cf1224cced52361315f8e63b484c243364e4
Philip Nduka <philip.igboba@mkobo.com.ng>
2023-06-17 15:40:54 +0100

    Initial Commit

---
+++ consolidateapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  consolidateapp
+//
+//  Created by APPLE on 6/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
